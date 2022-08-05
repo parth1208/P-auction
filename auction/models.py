e2995611b0bcb035fc225bbe9a0be68cefa7fe4e
malekzaid <zaidmalek6254@gmail.com>
2022-08-05 15:18:31 +0530

    product_model

--- auction/models.py
+++ auction/models.py
@@ -13,3 +13,29 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.username}"
+
+class Session_date(models.Model):
+    date = models.CharField(max_length=30,null=True)
+    def __str__(self):
+        return self.date
+        
+class Session_Time(models.Model):
+    date = models.ForeignKey(Session_date,on_delete=models.CASCADE,null=True)
+    time = models.CharField(max_length=30,null=True)
+    def __str__(self):
+        return self.date.date+" "+self.time
+
+class Status(models.Model):
+    status = models.CharField(max_length=30,null=True)
+    def __str__(self):
+        return self.status
+
+class Product(models.Model):
+    temp = models.IntegerField(null=True)
+    status  =models.ForeignKey(Status,on_delete=models.CASCADE,null=True)
+    name = models.CharField(max_length=100,null=True)
+    min_price = models.IntegerField(null=True)
+    images = models.FileField(null=True)
+    session = models.ForeignKey(Session_Time,on_delete=models.CASCADE,null=True)
+    def __str__(self):
+        return self.name
